4bc869d547a30e86ba47e3cb13acd4e9711eb390
John Lane <john.lane93@gmail.com>
2019-10-17 14:40:34 -0500

    update expression to include key

--- eve_dynamodb/expression.py
+++ eve_dynamodb/expression.py
@@ -5,35 +5,35 @@
 """
 
 from functools import reduce
-from boto3.dynamodb.conditions import Attr, ConditionBase
+from boto3.dynamodb.conditions import Attr, ConditionBase, Key
 
 
-def and_conditions(acc, val):
-    """
+def and_attr_conditions(acc: Attr, val: dict) -> ConditionBase:
+    """Logically 'AND' conditions together
 
-    :param acc:
-    :param val:
+    :param ConditionBase acc: Condition expression
+    :param dict val: Expression to combine
     :return:
     """
 
-    return (acc & build_expression(val)) if isinstance(acc, ConditionBase) else build_expression(val)
+    return (acc & build_attr_expression(val)) if isinstance(acc, ConditionBase) else build_attr_expression(val)
 
 
-def or_conditions(acc, val):
-    """
+def or_attr_conditions(acc: Attr, val: dict) -> ConditionBase:
+    """Logically 'OR' conditions together
 
-    :param acc:
-    :param val:
+    :param Attr acc: Condition expression
+    :param dict val: Expression to combine
     :return:
     """
 
-    return (acc | build_expression(val)) if isinstance(acc, ConditionBase) else build_expression(val)
+    return (acc | build_attr_expression(val)) if isinstance(acc, ConditionBase) else build_attr_expression(val)
 
 
-def build_expression(lookup, parent=None):
-    """Build an expression from a query
+def build_attr_expression(lookup: dict, parent: str = None) -> ConditionBase:
+    """Build an attr expression from a query
 
-    :param dict lookup:
+    :param dict lookup: Query expression
     :param str parent: Parent key
     :return:
     """
@@ -52,7 +52,7 @@ def build_expression(lookup, parent=None):
         '$exists': lambda key, value: Attr(key).exists() if value else Attr(key).not_exists(),
         '$size': lambda key, value: Attr(key).size().eq(value),
         '$startsWith': lambda key, value: Attr(key).begins_with(value),
-        '$type': lambda key, value: Attr(key).attribute_type(value),
+        '$type': lambda key, value: Attr(key).attribute_type(value)
     }
 
     logical_operators = ['$not', '$and', '$or', '$nor', '$xor']
@@ -63,29 +63,61 @@ def build_expression(lookup, parent=None):
         operator = operators.get(k, operators['$eq'])
 
         if isinstance(v, dict):
-            operations.append(build_expression(v, k))
+            operations.append(build_attr_expression(v, k))
 
         elif k in logical_operators and isinstance(v, (list, tuple)):
 
             if k == '$not':
-                operations.append(~reduce(and_conditions, v, None))
+                operations.append(~reduce(and_attr_conditions, v, None))
 
             elif k == '$and':
-                operations.append(reduce(and_conditions, v, None))
+                operations.append(reduce(and_attr_conditions, v, None))
 
             elif k == '$or':
-                operations.append(reduce(or_conditions, v, None))
+                operations.append(reduce(or_attr_conditions, v, None))
 
             elif k == '$nor':
-                operations.append(~reduce(or_conditions, v, None))
+                operations.append(~reduce(or_attr_conditions, v, None))
 
             elif k == '$xor' and len(v) == 2:
 
-                x, y = build_expression(v[0]), build_expression(v[1])
+                x, y = build_attr_expression(v[0]), build_attr_expression(v[1])
                 operations.append((x & ~y) | (~x & y))
 
             else:
                 raise ValueError("Error: $xor can only be computed against two values at a time")
+
+        else:
+            operations.append(operator(parent if parent else k, v))
+
+    return reduce(lambda acc, val: (acc & val) if acc else val, operations)
+
+
+def build_key_expression(lookup: dict, parent: str = None) -> ConditionBase:
+    """Build a key expression from a query
+
+    :param dict lookup: Query expression
+    :param str parent: Parent key
+    :return:
+    """
+
+    operators = {
+        '$eq': lambda key, value: Key(key).eq(value),
+        '$lt': lambda key, value: Key(key).lt(value),
+        '$lte': lambda key, value: Key(key).lte(value),
+        '$gt': lambda key, value: Key(key).gt(value),
+        '$gte': lambda key, value: Key(key).gte(value),
+        '$between': lambda key, value: Key(key).between(*value),
+        '$startsWith': lambda key, value: Key(key).begins_with(value)
+    }
+
+    operations = []
+
+    for k, v in lookup.items():
+        operator = operators.get(k, operators['$eq'])
+
+        if isinstance(v, dict):
+            operations.append(build_key_expression(v, k))
 
         else:
             operations.append(operator(parent if parent else k, v))
--- test/test_expression.py
+++ test/test_expression.py
@@ -5,8 +5,8 @@
 """
 
 import pytest
-from boto3.dynamodb.conditions import Attr
-from eve_dynamodb.expression import build_expression
+from boto3.dynamodb.conditions import Attr, Key
+from eve_dynamodb.expression import build_attr_expression, build_key_expression
 
 
 @pytest.mark.parametrize(('query', 'expectation'), (
@@ -27,15 +27,15 @@ from eve_dynamodb.expression import build_expression
         ({'foo': {'$startsWith': 'bar'}}, Attr('foo').begins_with('bar')),
         ({'foo': {'$type': 'number'}}, Attr('foo').attribute_type('number'))
 ))
-def test_single_expression(query, expectation):
-    """Test to ensure expressions are properly built from queries
+def test_single_attr_expression(query: dict, expectation: Attr):
+    """Test to ensure attr expressions are properly built from queries
 
     :param dict query: Query to build expression from
-    :param expectation: Expected expression
+    :param Attr expectation: Expected expression
     :return:
     """
 
-    assert build_expression(query) == expectation
+    assert build_attr_expression(query) == expectation
 
 
 @pytest.mark.parametrize(('query', 'expectation'), (
@@ -62,15 +62,15 @@ def test_single_expression(query, expectation):
                 Attr('x').attribute_type('int') & Attr('y').attribute_type('str')
         )
 ))
-def test_compound_expression(query, expectation):
-    """Test to ensure compound expressions are properly built from queries
+def test_attr_compound_expression(query: dict, expectation: Attr):
+    """Test to ensure attr compound expressions are properly built from queries
 
     :param dict query: Query to build expression from
-    :param expectation: Expected expression
+    :param Attr expectation: Expected expression
     :return:
     """
 
-    assert build_expression(query) == expectation
+    assert build_attr_expression(query) == expectation
 
 
 @pytest.mark.parametrize(('query', 'expectation'), (
@@ -80,15 +80,15 @@ def test_compound_expression(query, expectation):
                 Attr('foo').contains('bar') & Attr('foo').between('a', 'c')
         )
 ))
-def test_expression_with_multiple_conditions(query, expectation):
-    """Test to ensure expressions with multiple conditions are properly built from queries
+def test_attr_expression_with_multiple_conditions(query: dict, expectation: Attr):
+    """Test to ensure attr expressions with multiple conditions are properly built from queries
 
     :param dict query: Query to build expression from
-    :param expectation: Expected expression
+    :param Attr expectation: Expected expression
     :return:
     """
 
-    assert build_expression(query) == expectation
+    assert build_attr_expression(query) == expectation
 
 
 @pytest.mark.parametrize(('query', 'expectation'), (
@@ -101,12 +101,54 @@ def test_expression_with_multiple_conditions(query, expectation):
                 ((Attr('x').eq('foo') & ~Attr('y').eq('bar')) | (~Attr('x').eq('foo') & Attr('y').eq('bar')))
         )
 ))
-def test_expression_logical_operators(query, expectation):
-    """
+def test_attr_expression_logical_operators(query: dict, expectation: Attr):
+    """Test to ensure attr expressions can be built with logical operators
 
     :param dict query: Query to build expression from
-    :param expectation: Expected expression
+    :param Attr expectation: Expected expression
     :return:
     """
 
-    assert build_expression(query) == expectation
+    assert build_attr_expression(query) == expectation
+
+
+@pytest.mark.parametrize(('query', 'expectation'), (
+        ({'foo': 'bar'}, Key('foo').eq('bar')),
+        ({'foo': {'$eq': 'bar'}}, Key('foo').eq('bar')),
+        ({'foo': {'$lt': 1}}, Key('foo').lt(1)),
+        ({'foo': {'$lte': 2}}, Key('foo').lte(2)),
+        ({'foo': {'$gt': 3}}, Key('foo').gt(3)),
+        ({'foo': {'$gte': 4}}, Key('foo').gte(4)),
+        ({'foo': {'$between': [1, 3]}}, Key('foo').between(1, 3)),
+        ({'foo': {'$startsWith': 'bar'}}, Key('foo').begins_with('bar'))
+))
+def test_single_key_expression(query: dict, expectation: Key):
+    """Test to ensure key expressions are properly built from queries
+
+    :param dict query: Query to build expression from
+    :param Key expectation: Expected expression
+    :return:
+    """
+
+    assert build_key_expression(query) == expectation
+
+
+@pytest.mark.parametrize(('query', 'expectation'), (
+        ({'foo': 'bar', 'baz': 'hello'}, Key('foo').eq('bar') & Key('baz').eq('hello')),
+        ({'foo': {'$eq': 'bar'}, 'baz': {'$eq': 'world'}}, Key('foo').eq('bar') & Key('baz').eq('world')),
+        ({'foo': {'$lt': 1}, 'baz': {'$lt': 2}}, Key('foo').lt(1) & Key('baz').lt(2)),
+        ({'foo': {'$lte': 2}, 'baz': {'$lte': 3}}, Key('foo').lte(2) & Key('baz').lte(3)),
+        ({'foo': {'$gt': 3}, 'baz': {'$gt': 4}}, Key('foo').gt(3) & Key('baz').gt(4)),
+        ({'foo': {'$gte': 4}, 'baz': {'$gte': 5}}, Key('foo').gte(4) & Key('baz').gte(5)),
+        ({'x': {'$between': [1, 3], 'y': {'$between': [4, 5]}}}, Key('x').between(1, 3) & Key('y').between(4, 5)),
+        ({'x': {'$startsWith': 'a'}, 'y': {'$startsWith': 'b'}}, Key('x').begins_with('a') & Key('y').begins_with('b'))
+))
+def test_compound_expression(query: dict, expectation: Key):
+    """Test to ensure compound key expressions are properly built from queries
+
+    :param dict query: Query to build expression from
+    :param Key expectation: Expected expression
+    :return:
+    """
+
+    assert build_key_expression(query) == expectation
